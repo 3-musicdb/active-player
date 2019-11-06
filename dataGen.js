8961d080225032e6362b7e81f79269a46b96b3c4
Parteek S. Sandhu <ParteekSSandhu@MacBook-Pro-5.local>
2019-11-06 14:59:53 -0800

    Generate all test csv files

--- dataGen.js
+++ dataGen.js
@@ -4,12 +4,21 @@ const faker = require('faker');
 // const gzip = zlib.createGzip();
 // const inp = fs.createReadStream('songs.csv');
 // const out = fs.createWriteStream('songs.csv.gz');
-
-const writeSongs = fs.createWriteStream('songs.csv');
 // const compressSongs = gzip(data).pipe(writeSongs);
-writeSongs.write('songId,title,album,artist,likes,length\n', 'utf8');
 
-const writeData = (writer, encoding, callback) => {
+// const writeSongs = fs.createWriteStream('songs.csv');
+// writeSongs.write('songId,title,album,artist,likes,length\n', 'utf8');
+
+const writeGenres = fs.createWriteStream('genres.csv');
+writeGenres.write('genreId,name\n', 'utf8');
+
+// const writeArtists = fs.createWriteStream('artists.csv');
+// writeArtists.write('artistId,name\n', 'utf8');
+
+// const writeAlbums = fs.createWriteStream('albums.csv');
+// writeAlbums.write('albumId,name\n', 'utf8');
+
+const generateSongs = (writer, encoding, callback) => {
   let i = 100;
   let id = 0;
   const write = () => {
@@ -37,9 +46,117 @@ const writeData = (writer, encoding, callback) => {
   write();
 }
 
-writeData(writeSongs, 'utf-8', () => {
-  writeSongs.end();
+
+const generateGenres = (writer, encoding, callback) => {
+  let genreArray = [
+    'industrial',
+    'heavy metal',
+    'rock-n-roll',
+    'punk',
+    'alternative',
+    'pop',
+    'country',
+    'blues',
+    'gospel',
+    'jazz',
+    'jamaican',
+    'rap',
+    'breakbeat',
+    'drum-n-bass',
+    'hardcore',
+    'techno',
+    'house',
+    'trance',
+    'downtempo'
+  ]
+  let i = 100;
+  let id = 0;
+  const write = () => {
+    let ok = true;
+    do {
+      i--;
+      id++;
+      const element = Math.floor(Math.random() * 19);
+      const genreElement = genreArray[element];
+      const data = `${id},${genreElement}\n`;
+      if ( i === 0 ) {
+        writer.write(data, encoding, callback);
+      } else {
+        ok = writer.write(data, encoding);
+      }
+    }
+    while ( i > 0 && ok );
+    if ( i > 0 ) {
+      writer.once('drain', write);
+    }
+  }
+  write();
+}
+
+const generateArtists = (writer, encoding, callback) => {
+  let i = 100;
+  let id = 0;
+  const write = () => {
+    let ok = true;
+    do {
+      i--;
+      id++;
+      const firstName = faker.name.firstName();
+      const lastName = faker.name.lastName();
+      const data = `${id},${firstName},${lastName}\n`;
+      if ( i === 0 ) {
+        writer.write(data, encoding, callback);
+      } else {
+        ok = writer.write(data, encoding);
+      }
+    }
+    while ( i > 0 && ok );
+    if ( i > 0 ) {
+      writer.once('drain', write);
+    }
+  }
+  write();
+}
+
+const generateAlbums = (writer, encoding, callback) => {
+  let i = 100;
+  let id = 0;
+  const write = () => {
+    let ok = true;
+    do {
+      i--;
+      id++;
+      const album = faker.lorem.word();
+      const data = `${id},${album}\n`;
+      if ( i === 0 ) {
+        writer.write(data, encoding, callback);
+      } else {
+        ok = writer.write(data, encoding);
+      }
+    }
+    while ( i > 0 && ok );
+    if ( i > 0 ) {
+      writer.once('drain', write);
+    }
+  }
+  write();
+}
+
+// generateSongs(writeSongs, 'utf-8', () => {
+//   writeSongs.end();
+// });
+
+generateGenres(writeGenres, 'utf-8', () => {
+  writeGenres.end();
 });
+
+// generateArtists(writeArtists, 'utf-8', () => {
+//   writeArtists.end();
+// });
+
+// generateAlbums(writeAlbums, 'utf-8', () => {
+//   writeAlbums.end();
+// });
 
 // compress file after it has been created
 // inp.pipe(gzip).pipe(out)
--- database/data/schema.sql
+++ database/data/schema.sql
@@ -4,14 +4,34 @@ CREATE DATABASE soundcloutplayer;
 
 \c soundcloutplayer
 
+CREATE TABLE genre (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE albums (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE artists (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
 CREATE TABLE songs (
   id SERIAL PRIMARY KEY,
   songId INT NOT NULL,
   title TEXT NOT NULL,
   album TEXT NOT NULL,
   artist TEXT NOT NULL,
-  likes INT DEFAULT 0,
   length INT NOT NULL,
   timestamp INT DEFAULT 0
+  likes INT DEFAULT 0,
+  genre_id INT REFERENCES genre(id),
+  artist_id INT REFERENCES artists(id),
+  album_id INT REFERENCES albums(id),
 );
+
+
 
